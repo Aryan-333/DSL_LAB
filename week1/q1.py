56cb69b035b7bbcb710f5523da9994e1f45f82d5
Aryan-333 <79599835+Aryan-333@users.noreply.github.com>
2024-01-08 16:31:58 +0530

    Add files via upload

---
+++ week1/q1.py
@@ -0,0 +1,6 @@
+x = int(input('Enter length:'))
+y = int(input('Enter breadth:'))
+
+print("Area of rectangle %d"%(x*y))
+
+
---
+++ week1/q2.py
@@ -0,0 +1,10 @@
+x = 10
+y = 5
+print("number before swap",x,y)
+
+
+temp =x
+x=y
+y=temp
+
+print("number after swap",x,y)
